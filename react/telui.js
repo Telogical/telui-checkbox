49ef32f1bc927d15d8b93cf87dd45751cb520f9f
the-simian <harlinjesse@gmail.com>
2015-02-11 12:53:10 -0600

    remove dunnecessary log

--- react/telui.js
+++ react/telui.js
@@ -3,6 +3,4 @@ var TelUI = require('@telogical/telui-core');
 TelUI.Appearances.checkbox = require('./appearances/checkbox')(TelUI);
 TelUI.Checkbox = require('./widgets/checkbox')(TelUI);
 
-console.log('Checkbox', TelUI.Appearances);
-
 module.exports = TelUI;
